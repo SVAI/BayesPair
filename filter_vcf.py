4e3ea9269dddb8dbb77a2a3006177fdb26afb65a
VinceKumar <VinceKumar8@gmail.com>
2017-06-25 12:18:51 -0700

    Adding filter SNPs and maintain vcf file format functionality

--- filter_vcf.py
+++ filter_vcf.py
@@ -1,15 +1,29 @@
 import vcf
 
-def main(file_input, file_output):
+def write_filtered_csv(file_input, file_output, only_snps=True):
 
-    vcf_file = file_input
     results = ''
-    for rec in vcf.Reader(open(vcf_file,'r')):
-        results += '{},{},{},{}\n'.format(rec.CHROM, rec.POS, rec.REF, rec.ALT, rec.QUAL)
+    for rec in vcf.Reader(open(file_input,'r')) :
+        if only_snps:
+            if rec.is_snp:
+                results += '{},{},{},{}\n'.format(rec.CHROM, rec.POS, rec.REF, rec.ALT, rec.QUAL)
+        elif not only_snps: 
+            results += '{},{},{},{}\n'.format(rec.CHROM, rec.POS, rec.REF, rec.ALT, rec.QUAL)
 
     with open("{}.csv".format(file_output), "w") as text_file:
         text_file.write(results)
 
 
+def filter_only_snps_vcf(file_input, file_output, only_snps=True):
+    
+    vcf_reader = vcf.Reader(filename=file_input)
+    vcf_writer = vcf.Writer(open(file_output,'w'), vcf_reader)
+    for record in vcf_reader:
+        if record.is_snp:
+            vcf_writer.write_record(record)
+
+
+
+
 if __name__ == '__main__':
-    main()
+    filter_only_snps_vcf('','.vcf') # insert file_input and file_output names
